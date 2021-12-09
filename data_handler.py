c8bedbff2133b149432ab2617387f5725de9e8f3
Madeline <madeline.ahn@mail.utoronto.ca>
2021-12-08 22:47:10 -0500

    Added add_points and make_dicts
    Fixed the classes Dataset and Point, and added functions for them.
    Currently still an issue with make_dicts I need to fix, and need to finish docstrings.

--- data_handler.py
+++ data_handler.py
@@ -1,77 +1,113 @@
-"""TODO: ADD DOCSTRING"""
+"""CSC110: Cryptographer
 
-# import functions
+TODO: DOCSTRING, Copyright things
+"""
+
+# Import functions
 from dataclasses import dataclass
 import datetime
 import csv
 import os
 import glob
-import pandas
-
-
-def get_all_csv() -> None:
-    """Takes all of the .csv files in the folder and concatenates them into one .csv file.
-
-    """
-    path = os.getcwd()
-    all_files = glob.glob(os.path.join(path, "*.csv"))  # finds the path of the csv files.
-
-    csv_files = []
-    for file in all_files:  # puts all the csv files into one list.
-        csv = pd.read_csv(filename, index_col=None)
-        csv_files.append(csv)
-
-    merged_csv = pd.concat(csv_files, ignore_index=True)  # concatenates the csv files into one .csv file.
-    merged_csv.to_csv("all_coin.csv")  # exports the merged .csv file.
 
 
 class Point:
-    """Class that receives information for all of the cryptocurrencies for each day.
+    """A class containing information about one day of a cryptocurrency.
+
+    Instance Attributes:
+        - name: Name of the cryptocurrency.
+        - date: Date for that day's high/low/marketcap for the cryptocurrency.
+        - high: Highest value of the cryptocurrency that day.
+        - low: Lowest value of the cryptocurrency that day.
+        - marketcap: Total value of all the shares of the cryptocurrency that day.
 
     Representation Invariants:
-        -
-        -
-        -
+        - self.name != ''
     """
     name: str
-    date: datetime.datetime
+    date: datetime.date
     high: float
     low: float
     marketcap: float
 
+    def __init__(self, name, date, high, low, marketcap) -> None:
+        self.name = name
+        self.date = date
+        self.high = high
+        self.low = low
+        self.marketcap = marketcap
 
-# Make sure to do representation invariants for the class
+    def add_points(self) -> list[Point]:
+        """Opens all of the csv files and creates a list of lists for each point.
+
+        Preconditions:
+            -
+
+        Sample Usage:
+        >>>
+        """
+        path = os.getcwd()/"path"
+        all_files = glob.glob(os.path.join(path, "*.csv"))
+        points_so_far = []
+
+        for file in all_files:
+            with open(os.path.join(path, file)) as f:
+                data = csv.reader(f)
+                next(data)
+
+                for row in data:
+                    points_so_far.append(Point(row[1], row[3], row[4], row[5], row[9]))
+        return points_so_far
 
 
 @dataclass
 class Dataset:
     """Class that brings together objects from Point that have the same cryptocurrency name.
+
+    Instance Attributes:
+        - dataset: A dictionary containing the name of the cryptocurrency as the key,
+            and a list of the points of that cryptocurrency as the value.
+
+    Representation Invariants:
+        - dataset != {}
     """
+    dataset: dict[str, list[Point]]
 
+    def make_dicts(self, points_so_far: list[Point]) -> dict[str, list[Point]]:
+        """Turns the list of all the points from Point into an organized dictionary.
 
-def dictionary(x: Dataset) -> dict:
-    """Returns the dictionary of the dataset, with the name of the crypto as the key and
-    the dataset as its key.
-    """
-    x = {'0': 0}
+        Preconditions:
+            - points_so_far != []
 
-    return x
+        Sample Usage:
+        >>> TODO
 
+        """
+        dict_so_far = dict()
+        for point in points_so_far:
+            temp_list = [point]
+            if point.name in dict_so_far.keys():
+                dict_so_far[point.name] += temp_list
+            else:
+                dict_so_far[point.name] = temp_list
+        dict = Dataset(dict_so_far)  # need to fix this function and make it proper
+        # (unless you think this is fine, i think there's just better ways of doing this)
+        return dict
 
-if __name__ == '__main__':
-    import python_ta
-
-    python_ta.check_all(config={
-        'max-line-length': 100,
-        'extra-imports': ['python_ta.contracts', 'random', 'tkinter'],
-        'disable': ['R1705', 'C0200'],
-        'max-args': 7
-    })
-
-    import python_ta.contracts
-
-    python_ta.contracts.DEBUG_CONTRACTS = False
-    python_ta.contracts.check_all_contracts()
+# if __name__ == '__main__':
+#     import python_ta
+#
+#     python_ta.check_all(config={
+#         'max-line-length': 100,
+#         'extra-imports': ['python_ta.contracts', 'random', 'tkinter'],
+#         'disable': ['R1705', 'C0200'],
+#         'max-args': 7
+#     })
+#
+#     import python_ta.contracts
+#
+#     python_ta.contracts.DEBUG_CONTRACTS = False
+#     python_ta.contracts.check_all_contracts()
 
     import doctest
 
